5020b6fe5803f6989d8085b1a52c50c86b3b8edf
gofna <gofnaivry90@gmail.com>
2020-04-27 22:12:41 +0300

    fix test

--- Test.cpp
+++ Test.cpp
@@ -8,57 +8,57 @@ using namespace std;
 TEST_CASE("test for operator +") {
     RealVariable x;
     CHECK(solve(2+x+10 == 10) == -2);
-    CHECK(solve(x+20 == 1) == -2);
-    CHECK(solve(7+x == 14) == -2);
-    CHECK(solve(9.5 == 2.5+x) == -2);
-    CHECK(solve(1+x+2 == 8) == -2);
-    CHECK(solve(4+4+x == 21) == -2);
-    CHECK(solve(x+1+3.5 == 5.5) == -2);
-    CHECK(solve(222+x == 2555) == -2);
-    CHECK(solve(x+1.4 == 2.6) == -2);
-    CHECK(solve(5.8+x == 11.3) == -2);
-    CHECK(solve(x+2+3 == 4) == -2);
-    CHECK(solve(x+2+3.333 == 4.125) == -2);   
+    CHECK(solve(x+20 == 1) == -21);
+    CHECK(solve(7+x == 14) == 7);
+    CHECK(solve(9.5 == 2.5+x) == 7);
+    CHECK(solve(1+x+2 == 8) == 5);
+    CHECK(solve(4+4+x == 21) == 13);
+    CHECK(solve(x+1+3.5 == 5.5) == 1);
+    CHECK(solve(222+x == 2555) == 2333);
+    CHECK(solve(x+1.4 == 2.6) == 1.2);
+    CHECK(solve(5.88+x == 11.3) == 5.42);
+    CHECK(solve(x+2+3 == 4) == -1);
+    CHECK(solve(x+2+3.333 == 4.125) == -1.208);   
 }
 
 TEST_CASE("test for operator -") {
     RealVariable x;
-    CHECK(solve(2-10-x == 20) == -2);
-    CHECK(solve(1-x-20 == 1) == -2);
-    CHECK(solve(7-x-1 == 14) == -2);
-    CHECK(solve(9.5 == 2.5-x) == -2);
-    CHECK(solve(1-x-2.1 == 8) == -2);
-    CHECK(solve(4-4-x == 19.11) == -2);
-    CHECK(solve(x-1-3.5 == -5.5) == -2);
-    CHECK(solve(2212-x == 2.112) == -2);
-    CHECK(solve(x-1.444 == 2.6) == -2);
-    CHECK(solve(5.8-x == 11.3) == -2);
-    CHECK(solve(x+2+3 == -1) == -2);
-    CHECK(solve(x-2.21-3.90 == 2) == -2);
+    CHECK(solve(2-10-x == 20) == -32);
+    CHECK(solve(1-x-20 == 1) == -20);
+    CHECK(solve(7-x-1 == 14) == -8);
+    CHECK(solve(9.5 == 2.5-x) == -7);
+    CHECK(solve(1-x-2.1 == 8) == -9.1);
+    CHECK(solve(4-4-x == 19.11) == -19.11);
+    CHECK(solve(x-1-3.5 == -5.5) == -1);
+    CHECK(solve(2212-x == 2.112) == 2209.888);
+    CHECK(solve(x-1.444 == 2.6) == 4.044);
+    CHECK(solve(5.8-x == 11.3) == -5.5);
+    CHECK(solve(x+2+3 == -1) == -6);
+    CHECK(solve(x-2.21-3.90 == 2) == 8.11);
 }
 
 TEST_CASE("test for operator *") {
     RealVariable x;
-    CHECK(solve(10*x == 20) == -2);
-    CHECK(solve(x*10 == 1.2) == -2);
-    CHECK(solve(x*1.3 == 4.3) == -2);
-    CHECK(solve(2.5*x == 2.5) == -2);
-    CHECK(solve(3.45*x == 8) == -2);
-    CHECK(solve(8.38*x*3 == 19.11) == -2);
-    CHECK(solve(3*6*x == -5.5) == -2);
-    CHECK(solve(22.678*x == 2.112) == -2);
+    CHECK(solve(10*x == 20) == 2);
+    CHECK(solve(x*10 == 1.2) == 0.12);
+    CHECK(solve(x*1.3 == 4.3) == 3.3076);
+    CHECK(solve(2.5*x == 2.5) == 1);
+    CHECK(solve(3.45*x == 8) == 2.3188);
+    CHECK(solve(8.38*x*3 == 19.11) == 0.7601);
+    CHECK(solve(-3*6*x == -5.5) == 0.3055);
+    CHECK(solve(-22.678*x == 2.112) == -0.0931);
 }
 
 TEST_CASE("test for operator /") {
     RealVariable x;
     CHECK(solve(0/x == 2) == 0);
-    CHECK(solve(22.4/x == 1) == -2);
-    CHECK(solve(1/4/x == 14) == -2);
-    CHECK(solve(x/4.2 == 2.5-x) == -2);
-    CHECK(solve(4.387/x == 8) == -2);
-    CHECK(solve(x/2/6 == 19.11) == -2);
-    CHECK(solve(2.99/x == -5.5) == -2);
-    CHECK(solve(x/9.987 == 2.112) == -2);
+    CHECK(solve(22.4/x == 1) == 22.4);
+    CHECK(solve(1/4/x == 14) == 0.0178);
+    CHECK(solve(x/4.2 == 2.5-x) == 2.0192);
+    CHECK(solve(4.387/x == 8) == 0.5483);
+    CHECK(solve(x/2/6 == 19.11) == 6.37);
+    CHECK(solve(2.99/x == -5.5) == -0.5436);
+    CHECK(solve(x/9.987 == 2.112) == 21.0925);
     CHECK_THROWS(solve(x/0 == 2)); //can't divid by 0
 }
 
@@ -77,138 +77,137 @@ TEST_CASE("test for operator ^") {
 
 TEST_CASE("test for operators + and -") {
     RealVariable x;
-    CHECK(solve(x+2-6 == 2) == 0);
-    CHECK(solve(10+x-3 == 1) == -2);
+    CHECK(solve(x+2-6 == 2) == 6);
+    CHECK(solve(10+x-3 == 1) == -6);
     CHECK(solve(9-5+8-x == 14) == -2);
-    CHECK(solve(3+2-9.2 == 21.5-x) == -2);
-    CHECK(solve(x-41-6-9+20 == 8) == -2);
-    CHECK(solve(25+3-x+6 == -19.11) == -2);
-    CHECK(solve(9 == -5.5+x-8) == -2);
-    CHECK(solve(45 == x-2.112+67.45) == -2);
+    CHECK(solve(3+2-9.2 == 21.5-x) == 25.7);
+    CHECK(solve(x-41-6-9+20 == 8) == 44);
+    CHECK(solve(25+3-x+6 == -19.11) == 53.11);
+    CHECK(solve(9 == -5.5+x-8) == 22.5);
+    CHECK(solve(45 == x-2.112+67.45) == -20.338);
 }
 
 
 TEST_CASE("test for operators + and *") {
     RealVariable x;
-    CHECK(solve(x+2*6 == 2) == 0);
-    CHECK(solve(10+x*3 == 1) == -2);
-    CHECK(solve(9*5+8*x == 14) == -2);
-    CHECK(solve(3+2*9.2 == 21.5*x) == -2);
-    CHECK(solve(x*6*9+20 == 8) == -2);
-    CHECK(solve(25+3*x+6 == 19.11) == -2);
-    CHECK(solve(9 == -5.5+x*8) == -2);
-    CHECK(solve(45 == x*2.112+67.45) == -2);
+    CHECK(solve(x+2*6 == 2) == -10);
+    CHECK(solve(10+x*3 == 1) == -3);
+    CHECK(solve(9*5+8*x == 14) == -3.875);
+    CHECK(solve(3+2*9.2 == 21.5*x) == 0.9953);
+    CHECK(solve(x*6*9+20 == 8) == -18);
+    CHECK(solve(25+3*x+6 == 19.11) == -3.9633);
+    CHECK(solve(9 == -5.5+x*8) == 1.8125);
+    CHECK(solve(45 == x*2.112+67.45) == -10.6297);
 }
 
 TEST_CASE("test for operators + and /") {
    RealVariable x;
-    CHECK(solve(x+2/6 == 2) == 0);
-    CHECK(solve(10+x/3 == 1) == -2);
-    CHECK(solve(9-5+8/x == 14) == -2);
-    CHECK(solve(3+2-9.2 == 21.5/x) == -2);
-    CHECK(solve(x/41+6/9+20 == 8) == -2);
-    CHECK(solve(25+3/x+6 == -19.11) == -2);
-    CHECK(solve(9/3 == 5.5+x/8) == -2);
-    CHECK(solve(45 == x/2.112+67.45) == -2);
+    CHECK(solve(x+2/6 == 2) == 1.6666);
+    CHECK(solve(10+x/3 == 1) == -27);
+    CHECK(solve(9-5+8/x == 14) == 0.8);
+    CHECK(solve(3+2-9.2 == 21.5/x) == -5.1190);
+    CHECK(solve(x/41+6/9+20 == 8) == -519.3333);
+    CHECK(solve(25+3/x+6 == -19.11) == -7.4652);
+    CHECK(solve(9/3 == 5.5+x/8) == -20);
+    CHECK(solve(45 == x/2.112+67.45) == 3130.29);
 }
 
 TEST_CASE("test for operators + and ^") {
     RealVariable x;
-    CHECK(solve(x^2+6 == 2) == 0);
-    CHECK(solve(10+x^2 == 1) == -2);
-    CHECK(solve(9+8+x^2 == 14) == -2);
-    CHECK(solve(3+9.2 == 21.5+x^2) == -2);
-    CHECK(solve(x^2+41+20 == 8) == -2);
-    CHECK(solve(25+x^2 == 19.11) == -2);
-    CHECK(solve(9 == 5.5+x^2) == -2);
-    CHECK(solve(45 == x^2+2.112+67.45) == -2);
+    CHECK_THROWS(solve((x^2)+6 == 2)); // need to throw
+    CHECK_THROWS(solve(10+(x^2) == 1)); // need to throw
+    CHECK((solve(9+8+(x^2) == 140) == 11.0905 || solve(9+8+x^2 == 140) == -11.0905));
+    CHECK((solve(3+9.2 == 2.5+(x^2)) == 3.1144 || solve(3+9.2 == 2.5+(x^2)) == -3.1144));
+    CHECK((solve((x^2)+41*x+20 == 8) == -0.2948 || solve((x^2)+41*x+20 == 8) == -40.7051));
+    CHECK((solve(25+(x^2) == 39.11) == 3.7563 || solve(25+(x^2) == 39.11) == -3.7563));
+    CHECK((solve(9 == 5.5+(x^2)) == 1.8708 || solve(9 == 5.5+(x^2)) == -1.8708));
+    CHECK((solve(45 == (x^2)+2.112+67.45) == 5.5170 || solve(45 == (x^2)+2.112+67.45) == -5.5170));
 }
 
 TEST_CASE("test for operators - and *") {
     RealVariable x;
-    CHECK(solve(x*2-6 == 2) == 0);
-    CHECK(solve(10-x*2 == 1) == -2);
-    CHECK(solve(9-8-x*3.66 == 14*x) == -2);
-    CHECK(solve(3-9.2 == 21.5-x*5) == -2);
-    CHECK(solve(41*x-20 == 8) == -2);
-    CHECK(solve(25-x*45 == -19.11) == -2);
-    CHECK(solve(9 == -5.5-67.87*x) == -2);
-    CHECK(solve(45 == -2.112*x-67.45) == -2);
+    CHECK(solve(x*2-6 == 2) == 4);
+    CHECK(solve(10-x*2 == 1) == 4.5);
+    CHECK(solve(9-8-x*3.66 == 14*x) == 0.0566);
+    CHECK(solve(3-9.2 == 21.5-x*5) == 5.54);
+    CHECK(solve(41*x-20 == 8) == 0.6829);
+    CHECK(solve(25-x*45 == -19.11) == 0.9802);
+    CHECK(solve(9 == -5.5-67.87*x) == -0.2136);
+    CHECK(solve(45 == -2.112*x-67.45) == -53.2433);
 }
 
 TEST_CASE("test for operators - and /") {
     RealVariable x;
-    CHECK(solve(x/2-6 == 2) == 0);
-    CHECK(solve(10-x/2 == 1) == -2);
-    CHECK(solve(9-8-x/3.66 == 14) == -2);
-    CHECK(solve(3-9.2 == 21.5-x/5) == -2);
-    CHECK(solve(41/x-20 == 8) == -2);
-    CHECK(solve(25-x/45 == -19.11) == -2);
-    CHECK(solve(9 == -5.5-67.87/x) == -2);
-    CHECK(solve(-45 == -2.112/x-67.45) == -2);
+    CHECK(solve(x/2-6 == 2) == -8);
+    CHECK(solve(10-x/2 == 1) == 18);
+    CHECK(solve(9-8-x/3.66 == 14) == -47.58);
+    CHECK(solve(3-9.2 == 21.5-x/5) == 138.5);
+    CHECK(solve(41/x-20 == 8) == 25.125);
+    CHECK(solve(25-x/45 == -19.11) == 1984.95);
+    CHECK(solve(9 == -5.5-67.87/x) == -4.6806);
+    CHECK(solve(-45 == -2.112/x-67.45) == 67.4969);
 }
 
 TEST_CASE("test for operators - and ^") {
     RealVariable x;
-    CHECK(solve(x^2-6 == 2) == 0);
-    CHECK(solve(10-x^2 == 1) == -2);
-    CHECK(solve(9-8+x^2 == 14) == -2);
-    CHECK(solve(3-9.2 == 21.5-x^2) == -2);
-    CHECK(solve(x^2-41-20 == 8) == -2);
-    CHECK(solve(25-x^2 == -19.11) == -2);
-    CHECK(solve(-9 == -5.5-x^2) == -2);
-    CHECK(solve(45 == x^2-2.112-67.45) == -2);
+    CHECK((solve((x^2)-6 == 2) == 2.8284 || solve((x^2)-6 == 2) == -2.8284));
+    CHECK((solve(10-(x^2) == 1) == 3 || solve(10-(x^2) == 1) == -3));
+    CHECK((solve(9-8+(x^2) == 14) == 3.6055 || solve(9-8+(x^2) == 14) == -3.6055));
+    CHECK((solve(3-9.2 == 21.5-(x^2)) == 5.2630 || solve(3-9.2 == 21.5-(x^2)) == -5.2630));
+    CHECK((solve((x^2)-41-20 == 8) == 8.3066 || solve((x^2)-41-20 == 8) == -8.3066));
+    CHECK((solve(25-(x^2) == -19.11) == 6.6415 || solve(25-(x^2) == -19.11) == -6.6415));
+    CHECK((solve(-9 == -5.5-(x^2)) == 1.8708 || solve(-9 == -5.5-(x^2)) == -1.8708));
+    CHECK((solve(45 == (x^2)-2.112-67.45) == 10.7033 || solve(45 == (x^2)-2.112-67.45) == -10.7033));
 }
 
 TEST_CASE("test for operators * and /") {
     RealVariable x;
-    CHECK(solve(x*2/6 == 2) == 0);
+    CHECK(solve(x*2/6 == 2) == 6);
     CHECK(solve(10/x == 1*10) == 1);
-    CHECK(solve(9*8*x/2 == 14) == -2);
-    CHECK(solve(3/9.2 == 21.5*x) == -2);
-    CHECK(solve(x/41*20 == 8*21) == -2);
-    CHECK(solve(25/x*2 == 19.11) == -2);
-    CHECK(solve(9/0.5 == 5.5*x/2) == -2);
-    CHECK(solve(45 == x*2.112/67.45) == -2);
+    CHECK(solve(9*8*x/2 == 14) == 0.3888);
+    CHECK(solve(3/9.2 == 21.5*x) == 0.0151);
+    CHECK(solve(x/41*20 == 8*21) == 344.4);
+    CHECK(solve(25/x*2 == 19.11) == 2.6164);
+    CHECK(solve(9/0.5 == 5.5*x/2) == 6.5454);
+    CHECK(solve(45 == x*2.112/67.45) == 1437.1448);
 }
 
 TEST_CASE("test for operators * and ^") {
     RealVariable x;
-    CHECK(solve(x^2*1 == 2) == 0);
-    CHECK(solve(10*x^2 == 1*10) == 1);
-    CHECK(solve(9*8*x^2 == 14) == -2);
-    CHECK(solve(3*9.2 == 21.5*x^2) == -2);
-    CHECK(solve(x^2*20 == 8*21) == -2);
-    CHECK(solve(19.11 == 25*x^2) == -2);
-    CHECK(solve(9*2.5 == 5.5*x^2*3) == -2);
-    CHECK(solve(45 == x^2*2.112*67.45) == -2);
+    CHECK((solve((x^2)*1 == 2) == 1.4142 || solve((x^2)*1 == 2) == -1.4142));
+    CHECK((solve(10*(x^2) == 1*10) == 1 || solve(10*(x^2) == 1*10) == -1));
+    CHECK((solve(9*8*(x^2) == 14) == 0.4409 || solve(9*8*(x^2) == 14) == -0.4409));
+    CHECK((solve(3*9.2 == 21.5*(x^2)) == 1.1330 || solve(3*9.2 == 21.5*(x^2)) == -1.1330));
+    CHECK((solve((x^2)*20 == 8*21) == 2.8982 || solve((x^2)*20 == 8*21) == -2.8982));
+    CHECK((solve(19.11 == 25*(x^2)) == 0.8742 || solve(19.11 == 25*(x^2)) == -0.8742));
+    CHECK((solve(9*2.5 == 5.5*(x^2)*3) == 1.1677 || solve(9*2.5 == 5.5*(x^2)*3) == -1.1677));
+    CHECK((solve(45 == (x^2)*2.112*67.45) == 0.5620 || solve(45 == (x^2)*2.112*67.45) == -0.5620));
 }
 
 TEST_CASE("test for operators / and ^") {
     RealVariable x;
-    CHECK(solve(x^2/1 == 2) == 0);
-    CHECK(solve(10/x^2 == 1/10) == 1);
-    CHECK(solve(9/8/x^2 == 14) == -2);
-    CHECK(solve(3/9.2 == 21.5/x^2) == -2);
-    CHECK(solve(x^2/20 == 8/21) == -2);
-    CHECK(solve(19.11 == 25/x^2) == -2);
-    CHECK(solve(9/0.5 == 5.5/x^2/3) == -2);
-    CHECK(solve(45 == x^2/2.112) == -2);
+    CHECK((solve((x^2)/1 == 2) == 1.4142 || solve((x^2)/1 == 2) == -1.4142));
+    CHECK((solve(10/(x^2) == 1/10) == 10 || solve(10/(x^2) == 1/10) == -10));
+    CHECK((solve(9/8/(x^2) == 14) == 0.2834 || solve(9/8/(x^2) == 14) == -0.2834));
+    CHECK((solve(3/9.2 == 21.5/(x^2)) == 8.1199 || solve(3/9.2 == 21.5/(x^2)) == -8.1199));
+    CHECK((solve((x^2)/20 == 8/21) == 2.7602 || solve((x^2)/20 == 8/21) == -2.7602));
+    //CHECK(solve(19.11 == 25.0/(x^2.0)) == );
+    CHECK((solve(9/0.5 == 5.5/(x^2)/3) == 1.1437 || solve(9/0.5 == 5.5/(x^2)/3) == -1.1437));
+    CHECK_THROWS(solve(-45 == (x^2)/2.112));
 }
 
 
 TEST_CASE("test for all operators ") {
     RealVariable x;
-    CHECK(solve(x^2+7*x-1 == 2) == 0);
-    CHECK(solve(10*x^2+3 == -10) == 1);
-    CHECK(solve(9+8*x^2-3.7*x == 14) == -2);
-    CHECK(solve(3/9.2*x+43 == 21.5/x^2) == -2);
-    CHECK(solve(x^2/20+17.77 == 21-6.99) == -2);
-    CHECK(solve(-19.11 == 25*x^2+3*x+6) == -2);
-    CHECK(solve(9/0.5 == 5.5+x^2/3) == -2);
-    CHECK(solve(45 == x^2+2.112+67.45) == -2);
-    CHECK(solve(2 == x^2-2.112+67.45) == -2);
-
+    CHECK((solve((x^2)+7*x-1 == 2) == 0.4051 || solve((x^2)+7*x-1 == 2) == -7.4051));
+    CHECK((solve(10*(x^2)+3*x == -10) == 0.8611 || solve(10*x^2+3*x == -10) == -1.1611));
+    CHECK((solve(9+8*(x^2)-3.7*x == 14) == 1.0549 || solve(9+8*x^2-3.7*x == 14) == -0.5924));
+    CHECK((solve(3/9.2*x+43 == 21.5/(x^2)) == 70.3159 || solve(3/9.2*x+43 == 21.5/(x^2)) == -4.3826));
+    CHECK((solve((x^2)/20+17.77 == 21-6.99) == 23.0034 || solve((x^2)/20+17.77 == 21-6.99) == -23.0034));
+    CHECK((solve(19.11 == 25*(x^2)+3*x+6) == 0.6666 || solve(19.11 == 25*(x^2)+3*x+6) == -0.7866));
+    CHECK((solve(9/0.5 == 5.5+(x^2)/3) == 6.1237 || solve(9/0.5 == 5.5+(x^2)/3) == -6.1237));
+    CHECK((solve(4389 == (x^2)+2.112+67.45) == 65.7224 || solve(4389 == (x^2)+2.112+67.45) == -65.7224));
+    CHECK((solve(2 == (x^2)-209483.112+67.45) == 457.6217 || solve(2 == (x^2)-209483.112+67.45) == -457.6217));
 }
 
 
@@ -217,12 +216,12 @@ TEST_CASE("test for all operators ") {
 
 TEST_CASE("test for all operators complex") {
     ComplexVariable x;
-    CHECK(solve(4i+x^2 == 2.0) == 2i);
-    CHECK(solve(10.0*x-7 == -10) == 1.0);
-    CHECK(solve(x/-1.0*x == 14) == -2.0);
-    CHECK(solve(3/9.2*x == 21.5/x^2) == -2.2);
-    CHECK(solve(x^2/20+17.77 == 21-6.99) == -2i);
-    CHECK(solve(-19.11 == 25*x+3+6i) == -2i);
-    CHECK(solve(45 == x+2.112i+67.45/2-4i) == -2i);
-    CHECK(solve(2.0 == x^2-2.112+67.45) == 3i);
+    //CHECK(solve(4i+(x^2.0) == 2.0) == 2-4i);
+    CHECK(solve(10.0*x-7 == -10) == -0.3);
+    CHECK(solve(x/-1.0+x == 14i) == 0.9949-0.0710);
+    //CHECK(solve(3/9.2*x == 21.5/(x^2)) == -2.2);
+    CHECK((solve((x^2)/20+17.77 == 21-6.99) == 23.0034+0i || solve((x^2)/20+17.77 == 21-6.99) == -23.0034+0i));
+    CHECK(solve(-19.11 == 25*x+3+6i) == -0.8844-0.24i);
+    CHECK(solve(45 == x+2.112i+67.45/2-4i) == 22.55-182.112i);
+    //CHECK((solve((x^2) + 2*x + 4 == 20 + 6*x/2 - x) == 4+0i || solve((x^2) + 2*x + 4 == 20 + 6*x/2 - x) == -4+0i));
 }
